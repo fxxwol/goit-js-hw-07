64110bea19b825c1c6278e5f00e1a154b68b4885
fxxwol <protsivroksolana2004@gmail.com>
2023-04-18 13:00:39 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const makeGalleryItemMarkup = ({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</li>`
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('')
+gallery.innerHTML = makeGalleryMarkup
+
+gallery.addEventListener('click', onImgClick)
+function onImgClick(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    const instance = createInstance(e.target.dataset.source)
+
+    instance.show()
+
+    gallery.addEventListener('keydown', e => {
+        if (e.code === 'Escape') {
+            instance.close()
+        }
+    })
+}
+
+function createInstance(src) {
+    return basicLightbox.create(`
+    <img src="${src}">`)
+}
